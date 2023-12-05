a9d52d8a51a29cee35c5c5989b27eb2ffc4f5c2c
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2023-12-05 09:25:15 +0100

    feat: add @Injectable annotation and configure its scanning
    Created the custom @Injectable annotation to mark components within the application layer. Additionally, configured component scanning in Spring Boot to recognize this new annotation using custom filters in @ComponentScan.

--- src/main/java/com/seniorglez/demo/DemoApplication.java
+++ src/main/java/com/seniorglez/demo/DemoApplication.java
@@ -1,9 +1,19 @@
 package com.seniorglez.demo;
 
+import com.seniorglez.demo.application.Injectable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+		basePackages = "com.seniorglez.demo",
+		includeFilters = @ComponentScan.Filter(
+				type = FilterType.ANNOTATION,
+				classes = Injectable.class
+		)
+)
 public class DemoApplication {
 
 	public static void main(String[] args) {
